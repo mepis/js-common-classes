4bb4f0fcc02d3f5dc5cc99540cf8f468792c6572
mepis <mepis@users.noreply.github.com>
2023-11-07 10:50:07 -0500

    Added response messages class

---
+++ common/response_messages.js
@@ -0,0 +1,11 @@
+class response_messages {
+    constructor() {}
+    error(message) {
+      return { success: false, message: message };
+    }
+    success(message, data) {
+      return { success: true, message: message, data: data };
+    }
+  }
+  
+  module.exports = response_messages;
